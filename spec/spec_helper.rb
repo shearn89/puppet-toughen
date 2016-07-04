5a3d30d80fc38f08484a91e36a639da8f969e80a
shearna <alexander.shearn@gmail.com>
2016-07-04 15:13:48 +0100

    Adding codeclimate integration

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,15 @@
+require 'codeclimate-test-reporter'
+
+SimpleCov.formatters = [
+  SimpleCov::Formatter::HTMLFormatter,
+  CodeClimate::TestReporter::Formatter
+]
+
+SimpleCov.start do
+  add_filter '/spec/'
+  add_filter '/.vendor/'
+end
+
 require 'puppetlabs_spec_helper/module_spec_helper'
 
 RSpec.configure do |c|
