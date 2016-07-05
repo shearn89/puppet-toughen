858a96f880d782fda76664c3bd77f211fca5264f
shearna <alexander.shearn@gmail.com>
2016-07-05 09:57:52 +0100

    Adding HTML formatter

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,12 @@
 require 'codeclimate-test-reporter'
-CodeClimate::TestReporter.start
-
 require 'puppetlabs_spec_helper/module_spec_helper'
+
+SimpleCov.start do
+    formatter SimpleCov::Formatter::MultiFormatter.new([
+        SimpleCov::Formatter::HTMLFormatter,
+        CodeClimate::TestReporter::Formatter
+    ])
+end
 
 RSpec.configure do |c|
     c.default_facts = {
