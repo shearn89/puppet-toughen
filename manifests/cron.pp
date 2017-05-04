685b0aafd6e40617a98107ecb7faa967c2963b60
Alex Shearn <alex.shearn@bt.com>
2017-05-04 15:29:03 +0000

    Adding cron class

--- manifests/cron.pp
+++ manifests/cron.pp
@@ -1,4 +1,40 @@
 class toughen::cron (
-  $posture = 'default'
+  $allow_users = ['root']
 ){
+  service { 'crond':
+    ensure => 'running',
+    enable => true,
+  }
+
+  file { [
+      '/etc/crontab',
+      '/etc/cron.hourly',
+      '/etc/cron.daily',
+      '/etc/cron.weekly',
+      '/etc/cron.monthly'
+    ]:
+    ensure => file,
+    owner => 'root',
+    group => 'root',
+    mode => '0600',
+  }
+
+  file { '/etc/cron.d':
+    ensure => directory,
+    owner => 'root',
+    group => 'root',
+    mode => '0700',
+  }
+
+  file { ['/etc/cron.deny', '/etc/at.deny']:
+    ensure => absent,
+  }
+
+  file { ['/etc/cron.allow', '/etc/at.allow']:
+    ensure => file,
+    content => template('schedulers.allow.erb')
+    owner => 'root',
+    group => 'root',
+    mode => '0600',
+  }
 }
