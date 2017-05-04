6df12b5963c003b9e1c1cccee71177d7e6e53d6b
Alex Shearn <alex.shearn@bt.com>
2017-05-04 15:52:20 +0000

    Doh, enable should be false

--- manifests/services.pp
+++ manifests/services.pp
@@ -74,10 +74,19 @@ class toughen::services (
       }
 
       if $rpc_disabled {
-        service { 'rpcbind':
+        # Note there's a bug that means puppet doesn't understand 'indirect' services:
+	# https://tickets.puppetlabs.com/browse/PUP-6759
+        service { 'rpcbind.service':
           ensure => 'stopped',
-          enable => false,
+          enable => true,
+	  provider => 'systemd',
         }
+
+        service { 'rpcbind.socket':
+	  ensure => 'stopped',
+	  enable => false,
+	  provider => 'systemd',
+	}
       }
 
       if $dns_disabled {
