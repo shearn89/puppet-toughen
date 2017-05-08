bed38f7a115e1617dfb1e819ef26b618af2193aa
shearna <alexander.shearn@gmail.com>
2017-05-08 17:37:45 +0100

    Setting fact confine for debian builds

--- lib/facter/rpcbind_installed.rb
+++ lib/facter/rpcbind_installed.rb
@@ -7,3 +7,12 @@ Facter.add(:rpcbind_installed) do
     output.empty? ? false : true
   end
 end
+
+Facter.add(:rpcbind_installed) do
+  confine :osfamily => "Debian"
+
+  setcode do
+    output = Facter::Util::Resolution.exec('apt list rpcbind | grep rpcbind')
+    output.empty? ? false : true
+  end
+end
--- spec/unit/facter/rpcbind_installed_spec.rb
+++ spec/unit/facter/rpcbind_installed_spec.rb
@@ -2,7 +2,7 @@ describe 'rpcbind_installed', :type => :fact do
   before { Facter.clear }
   after { Facter.clear }
 
-  context "linux" do
+  context "redhat" do
     it "should return true if installed" do
       Facter::Util::Resolution.stubs(:exec).with("rpm -qa | grep rpcbind").returns('rpcbind')
       expect(Facter.fact(:rpcbind_installed).value).to eq(true)
