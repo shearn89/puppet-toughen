13b01fef48151d56481cb8a55dc7db082e2d3463
shearna <alexander.shearn@gmail.com>
2017-05-12 13:48:21 +0100

    Adding strange filesystem blacklist and a few other tweaks from scan

--- manifests/filesystem.pp
+++ manifests/filesystem.pp
@@ -3,14 +3,46 @@
 # Parameters
 # ----------
 # 
+# * `tmp_device`
+#  The /dev path for the fstab /tmp entry
+# 
 # * `tmp_options`
 #  The options to be provided to the /tmp mountpoint
 # 
 # * `tmp_mode`
 #  The numerical mode to be set on /tmp
 #
+# * `var_device`
+#  The /dev path for the fstab /var entry
+#
+# * `var_log_device`
+#  The /dev path for the fstab /var/log entry
+#
+# * `var_log_audit_device`
+#  The /dev path for the fstab /var/log/audit entry
+#
+# * `ramdisk_present`
+#  Whether there's a ramdisk present at all
+# 
 # * `ramdisk_options`
 #  The options to be provided to the /dev/shm mountpoint
+# 
+# * `fstype`
+#  The filesystem in use on e.g. /var, /tmp, etc.
+# 
+# * `blacklist_modules`
+#  The list of kernel modules to blacklist (/bin/true)
+# 
+# * `blacklist_confname`
+#  The name of the file under /etc/modprobe.d in which
+#  to place the blacklist. Note that this parameter will
+#  have '.conf' appended.
+# 
+# * `usb_disabled`
+#  Whether to add 'nousb' to the kernel params.
+# 
+# * `restrict_dmesg`
+#  Whether to restrict access to dmesg.
 # 
 class toughen::filesystem (
   $tmp_device = '/dev/mapper/rhel-tmp',
@@ -22,6 +54,18 @@ class toughen::filesystem (
   $ramdisk_present = false,
   $ramdisk_options = 'nodev,nosuid,noexec',
   $fstype = 'ext4',
+  $blacklist_modules = [
+    'usb-storage',
+    'cramfs',
+    'freevxfs',
+    'jffs2',
+    'hfs',
+    'hfsplus',
+    'squashfs',
+    'udf'],
+  $blacklist_confname = 'CIS',
+  $usb_disabled = true,
+  $restrict_dmesg = true,
 ){
 
   validate_re($tmp_options, '^[a-z,]+$')
@@ -70,7 +114,7 @@ class toughen::filesystem (
     ensure  => 'mounted',
     device  => '/tmp',
     fstype  => 'none',
-    options => 'bind',
+    options => 'rw,nodev,noexec,nosuid,bind',
   }
 
   if $ramdisk_present {
@@ -87,4 +131,22 @@ class toughen::filesystem (
     mode   => $tmp_mode,
   }
 
+  file { "/etc/modprobe.d/${blacklist_confname}.conf":
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0600',
+    content => template('toughen/modprobe.conf.erb'),
+  }
+
+  if $usb_disabled {
+    kernel_parameter { 'nousb':
+      ensure => present,
+    }
+  }
+  if $restrict_dmesg {
+    sysctl { 'kernel.dmesg_restrict':
+      value => 1
+    }
+  }
 }
