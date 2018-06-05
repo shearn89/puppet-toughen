10b9065647961c4c4a8ebf9974d127b262e7f974
shearn89 <alexander.shearn@gmail.com>
2018-06-05 09:37:41 +0100

    Updating spec_helper and gemfile.lock

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,18 +1,25 @@
 require 'simplecov'
+RSpec.configure do |c|
+  c.mock_with :mocha
+end
+
 require 'puppetlabs_spec_helper/module_spec_helper'
 
 SimpleCov.start do
-    add_filter "/vendor/"
-    add_filter "/spec/"
+  add_filter "/vendor/"
+  add_filter "/spec/"
 end
 
 RSpec.configure do |c|
-    c.default_facts = {
-        :osfamily => 'redhat',
-        :operatingsystem => 'CentOS',
-        :operatingsystemrelease => '7',
-        :operatingsystemmajrelease => '7',
-        :operatingsystemminrelease => '3',
-        :kernel => 'Linux'
-    }
+  c.default_facts = {
+      :osfamily => 'redhat',
+      :operatingsystem => 'CentOS',
+      :operatingsystemrelease => '7',
+      :operatingsystemmajrelease => '7',
+      :operatingsystemminrelease => '5',
+      :kernel => 'Linux'
+  }
+  # c.after(:suite) do
+  #   RSpec::Puppet::Coverage.report!
+  # end
 end
